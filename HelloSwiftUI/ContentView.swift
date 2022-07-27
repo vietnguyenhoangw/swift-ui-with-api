b2de811f1ee6b026eae9033b9617aa6a707bbaa1
vietnguyenhoangw <vietnguyenhoangw@gmail.com>
2022-07-27 13:05:50 +0700

    Initial Commit

---
+++ HelloSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloSwiftUI
+//
+//  Created by vietnguyenw on 7/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
